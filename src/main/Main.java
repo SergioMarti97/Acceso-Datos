01585cad03c82d5a8c33b1cdb72f751145d01a4f
Sergio Martí <sergio.martihee1997@gmail.com>
2023-02-25 18:57:27 +0100

    [END] finalizado proyecto

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,112 @@
 package main;
 
+import menu.MenuItem;
+import menu.MenuManager;
+import menu.MenuUtils;
+import properties.PropertiesUtils;
+
+import java.util.Properties;
+import java.util.Scanner;
+
+/**
+ * Este es la clase Main.
+ * Contiene la función "main", el punto de ejecución del programa.
+ */
 public class Main {
 
-    public static void main(String[] args) {
+    /**
+     * Nombre del archivo ".properties"
+     */
+    private static final String propertiesFileName = "app.properties";
 
+    /**
+     * Objeto properties
+     */
+    private static Properties properties;
+
+    /**
+     * Manejador del menu
+     */
+    public static MenuManager mm;
+
+    /**
+     * El scanner para leer el input del usuario por consola
+     */
+    public static Scanner scanner;
+
+    /**
+     * Esta función crea el menu del programa
+     */
+    public static void createMenu() {
+        mm = new MenuManager();
+        MenuItem mi = new MenuItem("Menu");
+        mi.add("Mostrar todos los nombres de las keys").setId(1);
+        mi.add("Mostrar el nombre del usuario").setId(2);
+        mi.add("Mostrar el email del usuario").setId(3);
+        mi.add("Cambiar idioma de " + propertiesFileName).setId(4);
+        mi.add("Salir").setId(-1);
+        mm.open(mi);
+    }
+
+    /**
+     * Esta función realiza una de las funcionalidades del programa
+     * @param id el id de la opción para seleccionar una funcionalidad
+     */
+    public static void manageAction(int id) {
+        switch (id) {
+            case 1: // Mostrar todos los nombres de las keys
+                for (var key : PropertiesUtils.getAllKeyNames(properties)) {
+                    System.out.println(key);
+                }
+                break;
+            case 2: // Mostrar el nombre del usuario
+                System.out.println(PropertiesUtils.getUserName(properties));
+                break;
+            case 3: // Mostrar el email del usuario
+                System.out.println(PropertiesUtils.getUserEmail(properties));
+                break;
+            case 4: // Cambiar el idioma en el archivo properties
+                System.out.print("Introduce el nuevo idioma (idioma actual: " + properties.getProperty("language") + "): ");
+                String newLanguage = scanner.nextLine();
+                PropertiesUtils.updateLang(properties, newLanguage);
+                break;
+        }
+    }
+
+    /**
+     * Función "main". Punto de ejecución del programa
+     * @param args argumentos del programa (en este caso no se utilizan)
+     */
+    public static void main(String[] args) {
+        // Instanciar el objeto scanner para poder leer la entrada por consola
+        scanner = new Scanner(System.in);
+        // Leer el archivo una sola vez, ya queda cacheado
+        properties = PropertiesUtils.getProperties(propertiesFileName);
+        // Añadir una nueva propiedad cuando se inicia el programa y se lee el archivo properties
+        PropertiesUtils.addPropertyAppStarted(properties);
+        // Crear el menu
+        createMenu();
+
+        // Bucle de la aplicación
+        MenuItem command;
+        do {
+            mm.show();
+            System.out.print("Introduza una opción: ");
+            command = mm.onConfirm(MenuUtils.getUserInputIntBetweenBounds(0, mm.getStack().peek().numChildren()) - 1);
+            System.out.println("Seleccionado: " + command.getName());
+            if (!command.hasChildren() && mm.getStack().size() > 1) {
+                mm.getStack().pop();
+            }
+            manageAction(command.getId());
+        } while (command.getId() != -1);
+
+        // Guardar las modificaciones del archivo properties
+        boolean isSaved = PropertiesUtils.saveProperties(properties, propertiesFileName);
+        if (isSaved) {
+            System.out.println("Se ha guardado el archivo: " + propertiesFileName);
+        } else {
+            System.out.println("No se ha podido guardar el arhivo: " + propertiesFileName);
+        }
     }
 
 }
--- src/properties/PropertiesUtils.java
+++ src/properties/PropertiesUtils.java
@@ -1,5 +1,11 @@
 package properties;
 
+import java.io.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.Properties;
+
 /**
  * Esta clase contiene los métodos que se piden en el ejercicio:
  * - Lectura de fichero properties
@@ -11,6 +17,94 @@ package properties;
  */
 public class PropertiesUtils {
 
+    /**
+     * Este método instancia un objeto Properties con la información del archivo indicado por parámetro
+     * @param fileName el nombre del archivo .properties
+     * @return objeto Properties
+     */
+    public static Properties getProperties(String fileName) {
+        Properties properties = null;
+        try {
+            properties = new Properties();
+            properties.load(new FileReader(fileName));
+        } catch (FileNotFoundException e) {
+            System.out.println("Archivo: " + fileName + " no encontrado");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("Error al leer el archivo: " + fileName);
+            e.printStackTrace();
+        }
+        return properties;
+    }
 
+    /**
+     * Este método devuelve una lista con los nombres de todas las "keys" presentes
+     * en el objeto properties pasado por parámetro.
+     * @param properties objeto properties
+     * @return una lista con los nombres de todas las "keys" presentes en el objeto properties
+     */
+    public static ArrayList<String> getAllKeyNames(Properties properties) {
+        ArrayList<String> keyNames = new ArrayList<>();
+        Enumeration<Object> keys = properties.keys();
+        while (keys.hasMoreElements()) {
+            Object key = keys.nextElement();
+            keyNames.add(key.toString());
+        }
+        return keyNames;
+    }
+
+    /**
+     * Este método devuelve el nombre del usuario presente en el archivo app.properties
+     * @param properties objeto properties
+     * @return el nombre del usuario presente en el archivo app.properties
+     */
+    public static String getUserName(Properties properties) {
+        return (String) properties.get("userName");
+    }
+
+    /**
+     * Este método devuelve el email del usuario presente en el archivo app.properties
+     * @param properties objeto properties
+     * @return el email del usuario presente en el archivo app.properties
+     */
+    public static String getUserEmail(Properties properties) {
+        return (String) properties.get("userEmail");
+    }
+
+    /**
+     * Este método cambia/actualiza la propiedad idioma (language) del archivo properties
+     * @param properties objeto properties
+     */
+    public static void updateLang(Properties properties, String newLanguage) {
+        properties.setProperty("language", newLanguage);
+    }
+
+    /**
+     * Este método añade al archivo app.properties el campo "appStarted", con el valor
+     * de la fecha actual
+     * @param properties objeto properties
+     */
+    public static void addPropertyAppStarted(Properties properties) {
+        properties.setProperty("appStarted", LocalDate.now().toString());
+    }
+
+    /**
+     * Este método guarda el objeto properties. Utiliza la función store, ya que save está deprecated
+     * @param properties objeto properties
+     * @return verdadero o falso según se ha podido o no guardar el archivo
+     */
+    public static boolean saveProperties(Properties properties, String fileName) {
+        try {
+            properties.store(new FileOutputStream(fileName), "Archivo: " + fileName);
+            return true;
+        } catch (FileNotFoundException e) {
+            System.out.println("Archivo: " + fileName + " no encontrado");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("Error al guardar el archivo " + fileName);
+            e.printStackTrace();
+        }
+        return false;
+    }
 
 }
